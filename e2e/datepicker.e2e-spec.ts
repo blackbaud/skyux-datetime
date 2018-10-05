6c73f996f733ad58c35b6bd28961d963fec01bd9
Conor Wright <conor.wright@blackbaud.com>
2018-10-05 17:16:11 -0400

    Initial port of datepicker and timepicker (#1)

---
+++ e2e/datepicker.e2e-spec.ts
@@ -0,0 +1,40 @@
+import {
+  expect,
+  SkyHostBrowser
+} from '@skyux-sdk/e2e';
+
+import {
+  element,
+  by
+} from 'protractor';
+
+describe('Datepicker', () => {
+  beforeEach(() => {
+    SkyHostBrowser.get('visual/datepicker');
+    SkyHostBrowser.setWindowBreakpoint('lg');
+  });
+
+  it('should match previous daypicker screenshot', (done) => {
+    expect('#screenshot-datepicker-calendar').toMatchBaselineScreenshot(done);
+  });
+
+  it('should match previous monthpicker screenshot', (done) => {
+    element(by.css('.sky-datepicker-calendar-title')).click();
+    expect('#screenshot-datepicker-calendar').toMatchBaselineScreenshot(done);
+  });
+
+  it('should match previous yearpicker screenshot', (done) => {
+    element(by.css('.sky-datepicker-calendar-title')).click();
+    element(by.css('.sky-datepicker-calendar-title')).click();
+    expect('#screenshot-datepicker-calendar').toMatchBaselineScreenshot(done);
+  });
+
+  it('should match previous datepicker input screenshot', (done) => {
+    expect('#screenshot-datepicker').toMatchBaselineScreenshot(done);
+  });
+
+  it('should match previous datepicker input screenshot when open', (done) => {
+    element(by.css('.sky-dropdown-button')).click();
+    expect('#screenshot-datepicker').toMatchBaselineScreenshot(done);
+  });
+});
