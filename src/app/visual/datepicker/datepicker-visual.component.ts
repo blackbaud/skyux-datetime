6ce5645b4b544cabe76b633e8c5e91d56d50ee02
BLACKBAUD\Trevor.Burch <Trevor.Burch@blackbaud.com>
2018-11-06 14:37:39 -0500

    Revert datepicker demo

--- src/app/visual/datepicker/datepicker-visual.component.ts
+++ src/app/visual/datepicker/datepicker-visual.component.ts
@@ -1,40 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'datepicker-visual',
   templateUrl: './datepicker-visual.component.html'
 })
-export class DatepickerVisualComponent implements OnInit {
-
-  public dateTimeForm: FormGroup;
-
-  public disable: boolean;
-
-  private dateControl: FormControl;
-  private timeControl: FormControl;
-
-  public ngOnInit() {
-    this.dateControl = new FormControl({ value: '', disabled: this.disable }, [Validators.required]);
-    this.timeControl = new FormControl({ value: '2:55 AM', disabled: this.disable }, [Validators.required]);
-    this.dateTimeForm = new FormGroup({
-      'date': this.dateControl,
-      'time': this.timeControl
-    });
-  }
-
-  public toggleDisable() {
-    this.disable = !this.disable;
-    if (this.disable) {
-      this.dateControl.disable();
-      this.timeControl.disable();
-    } else {
-      this.dateControl.enable();
-      this.timeControl.enable();
-    }
-  }
-
-  public printInfo() {
-    console.log(this.dateTimeForm.get('time').value);
-  }
+export class DatepickerVisualComponent {
+  public selectedDate: Date = new Date('4/4/2017');
 }
