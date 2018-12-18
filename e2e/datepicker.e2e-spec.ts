76c3dc00b569ba6cfd9f3b891338cde738d426a5
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-12-18 15:55:20 -0500

    Fixed visual tests

--- e2e/datepicker.e2e-spec.ts
+++ e2e/datepicker.e2e-spec.ts
@@ -15,26 +15,36 @@ describe('Datepicker', () => {
   });
 
   it('should match previous daypicker screenshot', (done) => {
-    expect('#screenshot-datepicker-calendar').toMatchBaselineScreenshot(done);
+    expect('#screenshot-datepicker-calendar').toMatchBaselineScreenshot(done, {
+      screenshotName: 'daypicker'
+    });
   });
 
   it('should match previous monthpicker screenshot', (done) => {
     element(by.css('.sky-datepicker-calendar-title')).click();
-    expect('#screenshot-datepicker-calendar').toMatchBaselineScreenshot(done);
+    expect('#screenshot-datepicker-calendar').toMatchBaselineScreenshot(done, {
+      screenshotName: 'monthpicker'
+    });
   });
 
   it('should match previous yearpicker screenshot', (done) => {
     element(by.css('.sky-datepicker-calendar-title')).click();
     element(by.css('.sky-datepicker-calendar-title')).click();
-    expect('#screenshot-datepicker-calendar').toMatchBaselineScreenshot(done);
+    expect('#screenshot-datepicker-calendar').toMatchBaselineScreenshot(done, {
+      screenshotName: 'yearpicker'
+    });
   });
 
   it('should match previous datepicker input screenshot', (done) => {
-    expect('#screenshot-datepicker').toMatchBaselineScreenshot(done);
+    expect('#screenshot-datepicker').toMatchBaselineScreenshot(done, {
+      screenshotName: 'datepicker-input'
+    });
   });
 
   it('should match previous datepicker input screenshot when open', (done) => {
     element(by.css('.sky-dropdown-button')).click();
-    expect('#screenshot-datepicker').toMatchBaselineScreenshot(done);
+    expect('#screenshot-datepicker').toMatchBaselineScreenshot(done, {
+      screenshotName: 'datepicker-input-open'
+    });
   });
 });
--- e2e/timepicker.e2e-spec.ts
+++ e2e/timepicker.e2e-spec.ts
@@ -15,20 +15,29 @@ describe('timepicker', () => {
   });
 
   it('should show the timepicker correctly for 12hr', (done) => {
-    expect('#screenshot-timepicker12hr').toMatchBaselineScreenshot(done);
+    expect('#screenshot-timepicker12hr').toMatchBaselineScreenshot(done, {
+      screenshotName: 'timepicker-12hr'
+    });
   });
 
   it('should show the timpicker correctly for 24hr', (done) => {
-    expect('#screenshot-timepicker24hr').toMatchBaselineScreenshot(done);
+    expect('#screenshot-timepicker24hr').toMatchBaselineScreenshot(done, {
+      screenshotName: 'timepicker-24hr'
+    });
   });
 
   it('should show the timepicker correctly after clicking on the icon for 12 hr', (done) => {
     element(by.css('#screenshot-timepicker12hr .sky-dropdown-button')).click();
-    expect('#screenshot-timepicker12hr .sky-popover-body').toMatchBaselineScreenshot(done);
+    expect('#screenshot-timepicker12hr').toMatchBaselineScreenshot(done, {
+      screenshotName: 'timepicker-12hr-open'
+    });
   });
 
-  it('should show the timepicker correctly after clicking on the icon for 24 hr', (done) => {
+  fit('should show the timepicker correctly after clicking on the icon for 24 hr', (done) => {
     element(by.css('#screenshot-timepicker24hr .sky-dropdown-button')).click();
-    expect('#screenshot-timepicker24hr .sky-popover-body').toMatchBaselineScreenshot(done);
+    SkyHostBrowser.scrollTo('#screenshot-timepicker24hr');
+    expect('#screenshot-timepicker24hr').toMatchBaselineScreenshot(done, {
+      screenshotName: 'timepicker-24hr-open'
+    });
   });
 });
