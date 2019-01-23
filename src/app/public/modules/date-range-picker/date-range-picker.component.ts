fc932237d2c5fed46cf8d98af098b2ae475709d7
Conor Wright <Conor.Wright@blackbaud.com>
2019-01-23 16:12:06 -0500

    added reactive demo and validation

--- src/app/public/modules/date-range-picker/date-range-picker.component.ts
+++ src/app/public/modules/date-range-picker/date-range-picker.component.ts
@@ -5,25 +5,40 @@ import {
 } from '@angular/core';
 
 import {
-  ControlValueAccessor, NG_VALUE_ACCESSOR
+  ControlValueAccessor, NG_VALUE_ACCESSOR, NG_VALIDATORS, AbstractControl
 } from '@angular/forms';
 
 import {
   SkyDateRangeDefaultValues
 } from './types/date-range-default-values';
-import { SkyDateRangeFormat } from './types';
+
+import {
+  SkyDateRangeFormat
+} from './types';
+import { SkyDateRangeFormatType } from './types/date-range-format-type';
+
+// tslint:disable:no-forward-ref no-use-before-declare
+const SKY_DATE_RANGE_PICKER_VALUE_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  // tslint:disable-next-line:no-forward-ref
+  useExisting: forwardRef(() => SkyDateRangePickerComponent),
+  multi: true
+};
+
+const SKY_DATE_RANGE_PICKER_VALIDATOR = {
+  provide: NG_VALIDATORS,
+  useExisting: forwardRef(() => SkyDateRangePickerComponent),
+  multi: true
+};
+// tslint:enable
 
 @Component({
   selector: 'sky-date-range-picker',
   templateUrl: './date-range-picker.component.html',
   styleUrls: ['./date-range-picker.component.scss'],
   providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      // tslint:disable-next-line:no-forward-ref
-      useExisting: forwardRef(() => SkyDateRangePickerComponent),
-      multi: true
-    }
+    SKY_DATE_RANGE_PICKER_VALUE_ACCESSOR,
+    SKY_DATE_RANGE_PICKER_VALIDATOR
   ]
 })
 export class SkyDateRangePickerComponent implements ControlValueAccessor {
@@ -38,10 +53,7 @@ export class SkyDateRangePickerComponent implements ControlValueAccessor {
   public set selectedFormat(value: SkyDateRangeFormat) {
     if (this.selectedFormat !== value) {
       this._selectedFormat = value;
-      this._onTouched();
-      this._onChange(
-        this.selectedFormat.getDateRangeValue(this.selectedFirstDate, this.selectedSecondDate)
-      );
+      this.updateValue();
     }
   }
   public get selectedFormat(): SkyDateRangeFormat {
@@ -51,10 +63,7 @@ export class SkyDateRangePickerComponent implements ControlValueAccessor {
   public set selectedFirstDate(value: Date) {
     if (this._selectedFirstDate !== value) {
       this._selectedFirstDate = value;
-      this._onTouched();
-      this._onChange(
-        this.selectedFormat.getDateRangeValue(this.selectedFirstDate, this.selectedSecondDate)
-      );
+      this.updateValue();
     }
   }
   public get selectedFirstDate(): Date {
@@ -91,18 +100,40 @@ export class SkyDateRangePickerComponent implements ControlValueAccessor {
     this.disabled = isDisabled;
   }
 
-  public registerOnChange(fn: (value: any) => any): void { this._onChange = fn; }
-  public registerOnTouched(fn: () => any): void { this._onTouched = fn; }
+  public validate(control: AbstractControl): {[key: string]: any} {
+    let value = control.value;
 
-  private updateValue() {
-    this._onTouched();
-    this._onChange(
-      this.selectedFormat.getDateRangeValue(this.selectedFirstDate, this.selectedSecondDate)
-    );
+    if (
+      value &&
+      this.selectedFormat.formatType === SkyDateRangeFormatType.SpecificRange &&
+      value.startDate && value.endDate &&
+      value.startDate > value.endDate
+    ) {
+      return {
+        'skyDateRange': {
+          invalid: control.value
+        }
+      };
+    }
+
+    return undefined;
   }
 
+  public registerOnChange(fn: (value: any) => any): void { this._onChange = fn; }
+  public registerOnTouched(fn: () => any): void { this.onTouched = fn; }
+  public registerOnValidatorChange(fn: () => void): void { this._validatorChange = fn; }
+
+  public onTouched = () => {};
   /*istanbul ignore next */
   private _onChange = (_: any) => {};
   /*istanbul ignore next */
-  private _onTouched = () => {};
+  private _validatorChange = () => {};
+
+  private updateValue() {
+    this.onTouched();
+    this._onChange(
+      this.selectedFormat.getDateRangeValue(this.selectedFirstDate, this.selectedSecondDate)
+    );
+    this._validatorChange();
+  }
 }
--- src/app/visual/date-range-picker/date-range-picker-visual.component.ts
+++ src/app/visual/date-range-picker/date-range-picker-visual.component.ts
@@ -1,13 +1,47 @@
 import {
-  Component
+  Component,
+  OnInit
 } from '@angular/core';
-import { SkyDateRangeValue } from '../../public';
+
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  FormControl
+} from '@angular/forms';
+
+import {
+  SkyDateRangeValue
+} from '../../public/modules/date-range-picker/types/date-range-value';
 
 @Component({
   selector: 'date-range-picker-visual',
   templateUrl: './date-range-picker-visual.component.html'
 })
-export class DateRangePickerVisualComponent {
-  public value: SkyDateRangeValue;
+export class DateRangePickerVisualComponent implements OnInit {
+  public value: SkyDateRangeValue = {};
   public disabled = false;
+  public reactiveForm: FormGroup;
+
+  public get reactiveRange(): AbstractControl {
+    return this.reactiveForm.get('selectedRange');
+  }
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  public ngOnInit(): void {
+    this.reactiveForm = this.formBuilder.group({
+      selectedRange: new FormControl({})
+    });
+
+    this.reactiveForm.statusChanges.subscribe((status: any) => {
+      console.log('Reactive date status:', status);
+    });
+
+    this.reactiveForm.valueChanges.subscribe((value: any) => {
+      console.log('Reactive date value:', value);
+    });
+  }
 }
