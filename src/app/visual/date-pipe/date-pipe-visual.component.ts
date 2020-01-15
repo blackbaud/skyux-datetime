f30baba134d51ce05725a3083a1331581644af82
Alex Kingman <alex.kingman@blackbaud.com>
2020-01-15 16:19:19 -0500

    cleaned up visuals

--- src/app/visual/date-pipe/date-pipe-visual.component.ts
+++ src/app/visual/date-pipe/date-pipe-visual.component.ts
@@ -13,8 +13,6 @@ import {
 })
 export class DatePipeVisualComponent implements OnInit {
 
-  public date = new Date('11/26/2020');
-
   public format: string = 'short';
 
   // Pre-defined format options from SkyDateFormatUtility.
@@ -46,6 +44,8 @@ export class DatePipeVisualComponent implements OnInit {
     'zh-CN'
   ];
 
+  public myDate = new Date('11/05/1955');
+
   constructor(
     private datePipe: SkyDatePipe
   ) { }
@@ -53,5 +53,14 @@ export class DatePipeVisualComponent implements OnInit {
   public ngOnInit(): void {
     const result = this.datePipe.transform(new Date('01/01/2019'), 'short', 'en-US');
     console.log('Result from calling pipe directly:', result);
+  }
+
+  public dateForDisplay(): string {
+    return JSON.stringify(this.myDate);
+  }
+
+  public onResetClick(): void {
+    this.locale = 'en-US';
+    this.format = 'short';
   }
 }
