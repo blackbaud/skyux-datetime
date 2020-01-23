3b5875de5c0b54121f03754bae8a7ebe262f6724
Alex Kingman <alex.kingman@blackbaud.me>
2020-01-23 10:16:32 -0500

    renamed e2e files to avoid build failures

--- e2e/fuzzy-datepicker.e2e-spec.ts
+++ e2e/fuzzy-datepicker.e2e-spec.ts
@@ -16,7 +16,7 @@ describe('Fuzzy datepicker', () => {
 
   it('should match previous daypicker screenshot', (done) => {
     expect('#screenshot-fuzzy-datepicker-calendar').toMatchBaselineScreenshot(done, {
-      screenshotName: 'daypicker'
+      screenshotName: 'fuzzy-datepicker-daypicker'
     });
   });
 
@@ -24,7 +24,7 @@ describe('Fuzzy datepicker', () => {
     element(by.css('.sky-datepicker-calendar-title')).click();
     SkyHostBrowser.moveCursorOffScreen();
     expect('#screenshot-fuzzy-datepicker-calendar').toMatchBaselineScreenshot(done, {
-      screenshotName: 'monthpicker'
+      screenshotName: 'fuzzy-datepicker-monthpicker'
     });
   });
 
@@ -33,7 +33,7 @@ describe('Fuzzy datepicker', () => {
     element(by.css('.sky-datepicker-calendar-title')).click();
     SkyHostBrowser.moveCursorOffScreen();
     expect('#screenshot-fuzzy-datepicker-calendar').toMatchBaselineScreenshot(done, {
-      screenshotName: 'yearpicker'
+      screenshotName: 'fuzzy-datepicker-yearpicker'
     });
   });
 
