a23445e67f0bbaabd16c539b247f238498c930b6
Erika McVey <erika.mcvey@blackbaud.com>
2020-03-16 15:41:56 -0400

    lets see if these fail again

--- src/app/public/modules/date-pipe/fixtures/date-pipe.component.fixture.ts
+++ src/app/public/modules/date-pipe/fixtures/date-pipe.component.fixture.ts
@@ -11,7 +11,7 @@ import {
   templateUrl: './date-pipe.component.fixture.html'
 })
 export class DatePipeTestComponent {
-  public dateValue: any = new Date('01/02/2000');
+  public dateValue: any = new Date('01/01/2000');
   public format: string;
   public locale: string;
 
