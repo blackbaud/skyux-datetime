b34ceea91b1ee0cd72722448e518ccb18dde8518
Paul Crowder <paul.crowder@blackbaud.com>
2020-08-26 14:41:14 -0400

    Added modern theme styles to timepicker

--- e2e/timepicker.e2e-spec.ts
+++ e2e/timepicker.e2e-spec.ts
@@ -1,6 +1,7 @@
 import {
   expect,
-  SkyHostBrowser
+  SkyHostBrowser,
+  SkyVisualThemeSelector
 } from '@skyux-sdk/e2e';
 
 import {
@@ -9,35 +10,94 @@ import {
 } from 'protractor';
 
 describe('timepicker', () => {
-  beforeEach(() => {
-    SkyHostBrowser.get('visual/timepicker');
-    SkyHostBrowser.setWindowBreakpoint('lg');
+  let currentTheme: string;
+  let currentThemeMode: string;
+
+  async function selectTheme(theme: string, mode: string): Promise<void> {
+    currentTheme = theme;
+    currentThemeMode = mode;
+
+    return SkyVisualThemeSelector.selectTheme(theme, mode);
+  }
+
+  function getScreenshotName(name: string): string {
+    if (currentTheme) {
+      name += '-' + currentTheme;
+    }
+
+    if (currentThemeMode) {
+      name += '-' + currentThemeMode;
+    }
+
+    return name;
+  }
+
+  function validateAll(): void {
+    it('should show the timepicker correctly for 12hr', (done) => {
+      expect('#screenshot-timepicker12hr').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('timepicker-12hr')
+      });
+    });
+
+    it('should show the timpicker correctly for 24hr', (done) => {
+      expect('#screenshot-timepicker24hr').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('timepicker-24hr')
+      });
+    });
+
+    it(
+      'should show the timepicker correctly after clicking on the icon for 12 hr',
+      async (done) => {
+        await element(by.css('#screenshot-timepicker12hr .sky-timepicker button')).click();
+
+        expect('#screenshot-timepicker12hr').toMatchBaselineScreenshot(done, {
+          screenshotName: getScreenshotName('timepicker-12hr-open')
+        });
+      }
+    );
+
+    it(
+      'should show the timepicker correctly after clicking on the icon for 24 hr',
+      async (done) => {
+        await element(by.css('#screenshot-timepicker24hr .sky-timepicker button')).click();
+
+        await SkyHostBrowser.scrollTo('#screenshot-timepicker24hr');
+
+        expect('#screenshot-timepicker24hr').toMatchBaselineScreenshot(done, {
+          screenshotName: getScreenshotName('timepicker-24hr-open')
+        });
+      }
+    );
+
+    it('should match previous timepicker input box screenshot', async (done) => {
+      await SkyHostBrowser.scrollTo('#screenshot-timepicker-input-box');
+
+      expect('#screenshot-timepicker-input-box').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('timepicker-input-box')
+      });
+    });
+  }
+
+  beforeEach(async () => {
+    await SkyHostBrowser.get('visual/timepicker');
+    await SkyHostBrowser.setWindowBreakpoint('lg');
   });
 
-  it('should show the timepicker correctly for 12hr', (done) => {
-    expect('#screenshot-timepicker12hr').toMatchBaselineScreenshot(done, {
-      screenshotName: 'timepicker-12hr'
+  validateAll();
+
+  describe('when modern theme', () => {
+    beforeEach(async () => {
+      await selectTheme('modern', 'light');
     });
+
+    validateAll();
   });
 
-  it('should show the timpicker correctly for 24hr', (done) => {
-    expect('#screenshot-timepicker24hr').toMatchBaselineScreenshot(done, {
-      screenshotName: 'timepicker-24hr'
+  describe('when modern theme in dark mode', () => {
+    beforeEach(async () => {
+      await selectTheme('modern', 'dark');
     });
-  });
 
-  it('should show the timepicker correctly after clicking on the icon for 12 hr', (done) => {
-    element(by.css('#screenshot-timepicker12hr .sky-timepicker button')).click();
-    expect('#screenshot-timepicker12hr').toMatchBaselineScreenshot(done, {
-      screenshotName: 'timepicker-12hr-open'
-    });
-  });
-
-  it('should show the timepicker correctly after clicking on the icon for 24 hr', (done) => {
-    element(by.css('#screenshot-timepicker24hr .sky-timepicker button')).click();
-    SkyHostBrowser.scrollTo('#screenshot-timepicker24hr');
-    expect('#screenshot-timepicker24hr').toMatchBaselineScreenshot(done, {
-      screenshotName: 'timepicker-24hr-open'
-    });
+    validateAll();
   });
 });
