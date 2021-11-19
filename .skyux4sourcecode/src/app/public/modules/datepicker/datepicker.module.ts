f27afa39b17f7ca2a08125d4de1d89b4590727a9
Alex Kingman <alex.kingman@blackbaud.me>
2021-11-19 15:20:53 -0500

    accidental removal

---
+++ .skyux4sourcecode/src/app/public/modules/datepicker/datepicker.module.ts
@@ -0,0 +1,98 @@
+import {
+  NgModule
+} from '@angular/core';
+
+import {
+  CommonModule
+} from '@angular/common';
+
+import {
+  FormsModule
+} from '@angular/forms';
+
+import {
+  SkyAffixModule,
+  SkyOverlayModule
+} from '@skyux/core';
+
+import {
+  SkyI18nModule
+} from '@skyux/i18n';
+
+import {
+  SkyIconModule
+} from '@skyux/indicators';
+
+import {
+  SkyThemeModule
+} from '@skyux/theme';
+
+import {
+  SkyDateTimeResourcesModule
+} from '../shared/datetime-resources.module';
+
+import {
+  SkyDatepickerCalendarComponent
+} from './datepicker-calendar.component';
+
+import {
+  SkyDatepickerCalendarInnerComponent
+} from './datepicker-calendar-inner.component';
+
+import {
+  SkyDayPickerComponent
+} from './daypicker.component';
+
+import {
+  SkyMonthPickerComponent
+} from './monthpicker.component';
+
+import {
+  SkyYearPickerComponent
+} from './yearpicker.component';
+
+import {
+  SkyDatepickerComponent
+} from './datepicker.component';
+
+import {
+  SkyDatepickerInputDirective
+} from './datepicker-input.directive';
+
+import {
+  SkyFuzzyDatepickerInputDirective
+} from './datepicker-input-fuzzy.directive';
+
+@NgModule({
+  declarations: [
+    SkyDatepickerCalendarComponent,
+    SkyDatepickerCalendarInnerComponent,
+    SkyDayPickerComponent,
+    SkyMonthPickerComponent,
+    SkyYearPickerComponent,
+    SkyDatepickerComponent,
+    SkyDatepickerInputDirective,
+    SkyFuzzyDatepickerInputDirective
+  ],
+  imports: [
+    CommonModule,
+    SkyI18nModule,
+    FormsModule,
+    SkyIconModule,
+    SkyDateTimeResourcesModule,
+    SkyAffixModule,
+    SkyOverlayModule,
+    SkyThemeModule
+  ],
+  exports: [
+    SkyDatepickerCalendarComponent,
+    SkyDatepickerCalendarInnerComponent,
+    SkyDayPickerComponent,
+    SkyMonthPickerComponent,
+    SkyYearPickerComponent,
+    SkyDatepickerComponent,
+    SkyDatepickerInputDirective,
+    SkyFuzzyDatepickerInputDirective
+  ]
+})
+export class SkyDatepickerModule { }
---
+++ .skyux4sourcecode/src/app/public/modules/datepicker/fixtures/datepicker-noformat.component.fixture.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'datepicker-no-format-test',
+  templateUrl: './datepicker-noformat.component.fixture.html'
+})
+export class DatepickerNoFormatTestComponent {
+
+  public selectedDate: any;
+}
