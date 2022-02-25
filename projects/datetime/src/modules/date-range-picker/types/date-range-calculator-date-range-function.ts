6b9c0c665ae8d83641424cd296128e4907f4b64c
Steve Brush <steve.brush@blackbaud.com>
2022-02-25 14:04:42 -0500

    Updated README.md to point contributors to the monorepo [ci skip] (#308)

--- projects/datetime/src/modules/date-range-picker/types/date-range-calculator-date-range-function.ts
+++
@@ -1,9 +0,0 @@
-import { SkyDateRange } from './date-range';
-
-/**
- * @internal
- */
-export type SkyDateRangeCalculatorGetValueFunction = (
-  startDateInput?: Date,
-  endDateInput?: Date
-) => SkyDateRange;
